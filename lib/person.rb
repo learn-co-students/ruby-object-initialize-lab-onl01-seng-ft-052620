81001339cc09cae7389cd363d4bba8b0b4c625ec
Michael_Harris <mwharris7@gmail.com>
2020-05-30 08:24:14 -0700

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+    
+    def initialize(name)
+        @name = name
+    end
+    
+    def name=(new_name)
+        @name = new_name
+    end
+    
+    def name
+        @name
+    end
+
+end
+
+
+# michael = Person.new("Prison Mike", "Full-Stack Web Developer")
+# puts michael.name
+# puts michael.job
+
+
